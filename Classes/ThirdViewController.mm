423a1c77a666a393d1c98ae5d3407a5ba53458b8
Tom Ilsinszki <tomilsinszki@yahoo.com>
2011-02-15 09:08:59 +0100

    Modify view controller and delegate extensions from .m to .mm

---
+++ Classes/ThirdViewController.mm
@@ -0,0 +1,31 @@
+//
+//  ThirdViewController.m
+//  RunTunes
+//
+//  Created by Tamás Ilsinszki on 2/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+
+@implementation ThirdViewController
+
+- (void)viewDidLoad {
+	[[self navigationController] setNavigationBarHidden:YES];
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+@end
