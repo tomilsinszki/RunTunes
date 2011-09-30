4ac43b40cc50b8a03a7b742f86e18e64fc163d4e
Tom Ilsinszki <tomilsinszki@yahoo.com>
2011-09-30 18:37:14 +0200

    Added background to running screen.

--- Classes/RunningViewController.mm
+++ Classes/RunningViewController.mm
@@ -34,13 +34,12 @@
 }
 
 #pragma mark - View lifecycle
-
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
+    UIColor *backgroundImage = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"runningBackground.png"]];
+    [[self view] setBackgroundColor:backgroundImage];
+    [backgroundImage release];
+    
     [super viewDidLoad];
 }
 
