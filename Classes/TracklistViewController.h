3612754789e50bb1eaea95f220fb59253bf2e94c
Tom Ilsinszki <tomilsinszki@yahoo.com>
2011-09-30 18:56:07 +0200

    Added tracklist view (still empty).

---
+++ Classes/TracklistViewController.h
@@ -0,0 +1,16 @@
+//
+//  TracklistViewController.h
+//  RunTunes
+//
+//  Created by Tamás Ilsinszki on 9/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TracklistViewController : UIViewController {
+    
+}
+
+@end
